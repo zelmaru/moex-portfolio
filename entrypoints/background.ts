d156bc8c18796efff1aac94f2c8b0881b2d3e9b1
zelmaru <zelenkovait@gmail.com>
2026-07-07 17:24:15 +0300

    use sidepanel instead of popup

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,7 @@
+import { browser } from "wxt/browser";
+// open the side panel when the user clicks the browser extension icon
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+	browser.sidePanel
+		.setPanelBehavior({ openPanelOnActionClick: true })
+		.catch((error: unknown) => console.error(error));
 });
